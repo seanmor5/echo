48014699fcfa48cf987372686837abccb29a0a9e
Sean Moriarity <smoriarity.5@gmail.com>
2024-02-25 08:47:56 -0500

    Fix?

--- lib/echo_web/router.ex
+++ lib/echo_web/router.ex
@@ -1,3 +1,15 @@
 defmodule EchoWeb.Router do
   use EchoWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", EchoWeb do
+    pipe_through :browser
+  end
 end
